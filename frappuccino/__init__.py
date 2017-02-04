b41e4a2cb67845d3b7e5e4b97a4fba0df9e2bff0
Matthias Bussonnier <bussonniermatthias@gmail.com>
2017-02-03 20:45:50 -0800

    'fist working version'

---
+++ frappuccino/__init__.py
@@ -0,0 +1,206 @@
+"""
+Frappucinno
+"""
+
+
+
+
+import inspect
+import sys
+import types
+import json
+
+from pprint import pprint
+from types import ModuleType
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.debug('HO')
+
+def fully_qualified(obj: object) -> str:
+    """
+    (try to) return the fully qualified name of an object
+    """
+    if obj is types.FunctionType: # noqa
+        return '%s.%s' % (obj.__module__, obj.__qualname__)
+    else:
+        return '%s.%s' % (obj.__class__.__module__, obj.__class__.__name__)
+
+class Visitor:
+
+    def __init__(self, name):
+        self.name = name
+
+        # list of visited nodes to avoid recursion and going in circle.
+        # can't be a set we store non-hashable objects
+        # which is weird why not store memory-location -> object ?
+        # anyway...
+        self.visited = list()
+
+        # set of object keys that where deemed worth collecting
+        self.collected = set({})
+
+
+        # dict of key -> custom spec that should be serialised for comparison
+        # later.
+        self.spec = dict()
+
+        # debug, make sure 2 objects are not getting the same key
+        self._consistency = {}
+
+    def _consistent(self, key, value):
+        if key in self._consistency:
+            if self._consistency[key] is not value:
+                print("Warning %s is not %s, results may not be consistent" % (self._consistency[key], value))
+        else:
+            self._consistency[key] = value
+
+
+
+    def visit(self, node):
+
+        if node in self.visited:
+            return
+        else:
+            self.visited.append(node)
+        mod = getattr(node, '__module__', None)
+        if mod and not mod.startswith(self.name):
+            # print('skipping | ', node.__module__, '|', node)
+            return
+
+        if isinstance(node, ModuleType):
+            type_ = 'module'
+        elif isinstance(node, object) and not isinstance(node, type) and not hasattr(node, '__call__'):
+            type_ = 'instance'
+        elif issubclass(type(node), type) and type(node) is not type:
+            type_ = 'metaclass_instance'
+        else:
+            type_ = type(node).__name__
+        visitor = getattr(self, 'visit_' + type_, self.visit_unknown)
+        return visitor(node)
+
+    def visit_metaclass_instance(self, meta_instance):
+        #print('visiting meta instance', meta_instance)
+        pass
+
+    def visit_unknown(self, unknown):
+
+        print('Unknown: ========')
+        print('Unknown: No clue what to do with', unknown)
+        print('Unknown: isinstance(node, object)', isinstance(unknown, object))
+        print('Unknown: isinstance(node, type)', isinstance(unknown, type))
+        print('Unknown: type(node)', type(unknown))
+        if type(unknown) is type:
+            print('Unknown: issubclass(unknown, type)', issubclass(unknown, type))
+        print('Unknown: issubclass(type(unknown), type)', issubclass(type(unknown), type), type(unknown))
+        print('Unknown: type(unknown) is type : ', type(unknown) is type)
+        print('Unknown: hasattr(unknown, "__call__"): ', hasattr(unknown, "__call__"))
+        print('Unknown: ========')
+
+    def visit_function(self, function):
+        klass = function.__class__
+        if klass is types.FunctionType:
+            name = function.__module__
+        else:
+            name = '%s.%s' % (function.__class__.__module__, function.__class__.__name__)
+        fullqual = '{}.{}'.format(name, function.__qualname__)
+        sig = str(inspect.signature(function))
+        logger.debug('    {f}{s}'.format(f=fullqual, s=sig))
+        self.collected.add(fullqual)
+        self.spec[fullqual] = sig
+        self._consistent(fullqual, function)
+
+    def visit_instance(self, instance):
+        #print('    vis instance', instance)
+        pass
+
+
+    def visit_type(self, type_):
+        logger.debug('Class %s' % type_.__module__ + '.' + type_.__name__)
+        for k, v in sorted(type_.__dict__.items()):
+            if not k.startswith('_'):
+                self.visit(v)
+
+
+    def visit_module(self, module):
+        logger.debug('Module %s' % module)
+        if not module.__name__.startswith(self.name):
+            return None
+        items = module.__dict__
+        for k, v in sorted(items.items()):
+            if k.startswith('_'):
+                continue
+            else:
+                self.visit(v)
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save', action='store_true')
+    parser.add_argument('--compare', action='store_true')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('modules', nargs='*')
+
+    argparse.ArgumentParser.add_argument
+
+    options = parser.parse_args(sys.argv)
+    options.modules.remove('__init__.py')
+
+    if options.save and options.compare:
+        print('options `--save` and `--compare` are exclusive')
+        parser.print_help()
+
+    if options.debug:
+        logger.debug('before')
+        logger.setLevel('DEBUG')
+        logger.debug('after')
+
+
+    print("let's go")
+    if len(sys.argv) > 1:
+        module_name = sys.argv[1]
+    else:
+        module_name = 'IPython'
+
+    module = __import__(module_name)
+
+    V = Visitor(module_name)
+    V.visit(module)
+    print("Visited", len(V.visited), "objects")
+    print("Collected", len(V.collected), "items")
+    if options.save:
+        with open('%s.json' % module_name, 'w') as f:
+            f.write(json.dumps(V.spec, indent=2))
+    if not options.save:
+        with open('%s.json' % module_name, 'r') as f:
+            loaded = json.loads(f.read())
+
+        lkeys = set(loaded.keys())
+        skeys = set(V.spec.keys())
+
+        common_keys = skeys.intersection(lkeys)
+        removed_keys = lkeys.difference(skeys)
+        new_keys = skeys.difference(lkeys)
+
+        print("The following items are new")
+        pprint(new_keys)
+        print()
+
+        print("The following items have been removed")
+        pprint(removed_keys)
+        print()
+
+        print("The following signature differ between versions:")
+        for key in common_keys:
+            l = loaded[key]
+            s = V.spec[key]
+            if l != s:
+                print()
+                print("         %s%s" % (key,l))
+                print("current> %s%s" % (key,s))
+
+
---
+++ frappuccino/astinit.py
@@ -0,0 +1,154 @@
+"""Frappucinno
+
+Freeze your API and make sure you do not introduce backward incompatibilities
+"""
+import ast
+from pprint import pprint
+__version__ = '0.0.1'
+
+test1 = """
+
+
+class Bird:
+    def bar(a,b, *args, kow, **kw ):
+        pass
+
+    def foo(c):
+        pass
+
+
+    def missing():
+        pass
+
+    def _private():
+        pass
+"""
+
+test2 = """
+class Bird:
+    def bar(a,b, *args, kow, **kw ):
+        pass
+
+    def foo(c):
+        pass
+
+    def _private():
+        pass
+"""
+
+def keyfy(s):
+    return '"%s":' % s
+
+class APIVisitor:
+    """
+    A node visitor base class that walks the abstract syntax tree and calls a
+    visitor function for every node found.  This function may return a value
+    which is forwarded by the `visit` method.
+
+    This class is meant to be subclassed, with the subclass adding visitor
+    methods.
+
+    Per default the visitor functions for the nodes are ``'visit_'`` +
+    class name of the node.  So a `TryFinally` node visit function would
+    be `visit_TryFinally`.  This behavior can be changed by overriding
+    the `visit` method.  If no visitor function exists for a node
+    (return value `None`) the `generic_visit` visitor is used instead.
+
+    Don't use the `NodeVisitor` if you want to apply changes to nodes during
+    traversing.  For this a special visitor exists (`NodeTransformer`) that
+    allows modifications.
+    """
+
+    def visit(self, node):
+        """Visit a node."""
+        method = 'visit_' + node.__class__.__name__
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        """Called if no explicit visitor function exists for a node."""
+        res = []
+        for field, value in ast.iter_fields(node):
+            if isinstance(value, list):
+                for item in value:
+                    vv = self.visit(item)
+                    res.append(vv)
+            elif isinstance(value, dict):
+                for k,v in value:
+                    res.append(self.visit(v))
+                res.append(self.visit(value))
+        if not res:
+            print("visiting", node)
+        return list(filter(None, res))
+
+    def visit_FunctionDef(self, node):
+        if node.name.startswith('_'):
+            return None
+        args = node.args
+        return {node.name: {'type': node.__class__.__name__,
+                            'args': [a.arg for a in args.args],
+                            'kwonlyargs': [a.arg for a in args.kwonlyargs],
+                            'vararg': args.vararg.arg if args.vararg else [],
+                            'kwarg': args.kwarg.arg if args.kwarg else [],
+                            }}
+
+    def visit_ClassDef(self, node):
+        vis = self.generic_visit(node)
+        d = {}
+        for item in vis:
+            d.update(item)
+        return {node.name: {'type': node.__class__.__name__, 'attributes': d}}
+
+
+def is_compatible(old_tree, new_tree):
+    pass
+
+
+class DoubleTreeVisitor:
+    """
+    Like AstVisitor, but compare two tree for compatibility. 
+    """
+
+    def visit(self, old_list, new_list, name=None):
+        """Visit a node."""
+        for old_node, new_node in zip(old_list, new_list):
+            for k, v in old_node.items():
+                if k in new_node:
+                    method = 'visit_' + v['type']
+                    visitor = getattr(self, method, self.generic_visit)
+                    visitor(v, new_node[k], k)
+
+    def generic_visit(self, old_node, new_node):
+        """Called if no explicit visitor function exists for a node."""
+        res = []
+        for field, value in old_node.items():
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, ast.AST):
+                        res.append(self.visit(item))
+            elif isinstance(value, ast.AST):
+                res.append(self.visit(value))
+        return list(filter(None, res))
+
+
+    def visit_ClassDef(self, old_class, new_class, name):
+        missing_attributes = set(old_class['attributes'].keys()).difference(set(new_class['attributes'].keys()))
+        if missing_attributes:
+            print('Class `{}` has lost non deprecated and non private following attributes : {}'.format(name,  *missing_attributes))
+        
+        self.generic_visit(old_class, new_class)
+
+
+
+
+if __name__ == '__main__':
+    tree = ast.parse(test1)
+    serialized_tree = APIVisitor().visit(tree)
+    
+    # pprint(serialized_tree)
+
+    tree2 = ast.parse(test2)
+    serialized_tree2 = APIVisitor().visit(tree2)
+
+
+    dt = DoubleTreeVisitor().visit(serialized_tree, serialized_tree2)
