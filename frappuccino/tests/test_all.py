9c3af6230633ea1b52ef23821bcbdc33bddd33bd
Dan Allan <dallan@bnl.gov>
2019-07-14 11:37:38 -0500

    Apply black.

--- frappuccino/tests/test_all.py
+++ frappuccino/tests/test_all.py
@@ -6,11 +6,8 @@ import json
 from frappuccino import main, visit_modules, compare
 
 
-def fix_spec(spec, old:str, new:str):
-    return {
-        k.replace(old,new):v
-        for k,v in spec.items()
-    }
+def fix_spec(spec, old: str, new: str):
+    return {k.replace(old, new): v for k, v in spec.items()}
 
 
 def test_old_new():
@@ -20,16 +17,15 @@ def test_old_new():
     assert skipped_o == []
     assert skipped_n == []
 
-    
-    
-    old_spec = fix_spec(old_spec_visitor.spec, 'frappuccino.tests.old','tests')
-    new_spec = fix_spec(new_spec_visitor.spec, 'frappuccino.tests.new','tests')
-
+    old_spec = fix_spec(old_spec_visitor.spec, "frappuccino.tests.old", "tests")
+    new_spec = fix_spec(new_spec_visitor.spec, "frappuccino.tests.new", "tests")
 
     l = list(compare(old_spec, spec=new_spec))
     assert json.dumps(old_spec) is not "{}"
-    assert l == [('The following signatures differ between versions:',),
-            None,
-            ('    tests.changed',),
-            ('          - tests.changed(a, b, c)',),
-            ('          + tests.changed(x, b, c)',)]
+    assert l == [
+        ("The following signatures differ between versions:",),
+        None,
+        ("    tests.changed",),
+        ("          - tests.changed(a, b, c)",),
+        ("          + tests.changed(x, b, c)",),
+    ]
